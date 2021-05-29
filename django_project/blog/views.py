f53cb4fbfd58dc8a0bef82db20a1a27611561f96
Hussen <hussenmohammedak@gmail.com>
2021-05-28 22:45:27 -0400

    Added Post model and created a database

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,25 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
-posts = [
-    {
-        'author': 'Hussen',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'May 28, 2021'
-    },
-    {
-        'author': 'Chris',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'May 29, 2021'
-    }
-]
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
